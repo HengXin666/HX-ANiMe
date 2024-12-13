46d3afca5efb80de558883cabe24ba438e36c23e
Heng_Xin_666 <282000500@qq.com>
2024-12-13 11:26:35 +0800

    pyAPI: 可以进行爬虫并且向后端提交数据, 并且提供对应的爬虫示例

--- HX-ANiMe-PyApi/getANiMeCVData.py
+++ HX-ANiMe-PyApi/getANiMeCVData.py
@@ -236,6 +236,11 @@ class HX_ANiMe_Api:
     
 # } // === End === HXANiMe-py-class === End ===
 
+class ANiMeData:
+    def __init__(self, jpName: str = "", cnName: str = "", cvName: str = ""):
+        self.jpName = jpName
+        self.cnName = cnName
+        self.cvName = cvName
 
 def getSubject(subjectId: int):
     """获取主题
@@ -260,10 +265,8 @@ def getCharactersSubject(subjectId: int):
     url = f"https://bangumi.tv/subject/{subjectId}/characters"
     res = requests.get(url=url, headers=UserAgent)
     res.encoding = 'utf-8'  # 确保正确解码
-    str = res.text  # 使用 .text 获取解码后的 HTML 内容
-    print(str)
-    with open(f'./{subjectId}characters.html', 'a+', encoding='utf-8') as f:
-        f.write(str)
+    # 使用 .text 获取解码后的 HTML 内容
+    return res.text
 
 def getANiMeDataBySubjectHtml(html: str):
     """正则表达式解析[Subject]HTML获取番剧的角色名称、CV信息
@@ -286,7 +289,7 @@ def getANiMeDataBySubjectHtml(html: str):
         japanese_name, chinese_name, cv_name = match
         print(f"日文名: {japanese_name}, 中文名: {chinese_name}, CV: {cv_name}")
 
-def getANiMeDataByCharactersSubjectHtml(html: str):
+def getANiMeDataByCharactersSubjectHtml(html: str) -> list[ANiMeData]:
     """正则表达式解析[Subject的Characters]HTML获取番剧的角色名称、CV信息
 
     Args:
@@ -296,16 +299,27 @@ def getANiMeDataByCharactersSubjectHtml(html: str):
         r'<h2><a href="[^"]*" class="l">([^<]+)</a> <span class="tip"> / ([^<]+)</span></h2>.*?<p><a href="[^"]*" class="l">([^<]+)</a>',
         re.DOTALL
     )
-
-    # 匹配内容
     matches = pattern.findall(html)
 
-    # 输出结果
+    res:list[ANiMeData] = []
     for match in matches:
         japanese_name, chinese_name, cv_name = match
-        print(f"日文名: {japanese_name}, 中文名: {chinese_name}, CV: {cv_name}")
+        res.append(ANiMeData(japanese_name, chinese_name, cv_name))
+    return res
 
-def main(ANiMeUrl: str, apiKey: str) -> None:
+def getANiMeTitleByCharactersSubjectHtml(html: str):
+    """正则表达式解析[Subject的Characters]HTML获取番剧名称
+
+    Args:
+        html (str): 网页HTML
+    """
+    pattern = re.compile(
+        r'<title>(.*?)的角色介绍</title>',
+        re.DOTALL
+    )
+    return pattern.findall(html)[0]
+
+def main(ANiMeUrl: str, apiKey: str, idNums: list[int]) -> None:
     HXANiMeHeaders = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0',
         "apiKey": apiKey,
@@ -320,7 +334,34 @@ def main(ANiMeUrl: str, apiKey: str) -> None:
         | legendMap.ensure(api, Legend(name="角色", color="#0099FF"))):
         # 有添加图例, 所以更新 legendMap
         legendMap = LegendIdMap(api.getLegends())
+    
+    # 爬取需要的内容, 然后存入这里
+    # nodeList: list[Node] = []
+    # edgeList: list[Edge] = []
+
+    for num in idNums:
+        # 爬取
+        html = getCharactersSubject(num)
+
+        # 解析 番剧名称
+        title = getANiMeTitleByCharactersSubjectHtml(html)
         
+        # 解析 角色 - CV 信息
+        animeList = getANiMeDataByCharactersSubjectHtml(html)
+
+        # 添加结点: 番剧信息结点; 并且获得结点id
+        titleNodeId = api.addNode(Node(legendId=legendMap["番剧"].legendId, name=title))
+
+        for it in animeList:
+            # 添加CV结点
+            cvNodeId = api.addNode(Node(legendId=legendMap["声优"].legendId, name=it.cvName))
+            # 添加角色结点
+            roleNodeId = api.addNode(Node(legendId=legendMap["角色"].legendId, name=f"{it.cnName} ({it.jpName})"))
+            # 添加边: 番剧 -> 角色
+            api.addEdge(Edge(fromNodeId=titleNodeId, toNodeId=roleNodeId))
+            # 添加边: 声优 -> 角色
+            api.addEdge(Edge(fromNodeId=cvNodeId, toNodeId=roleNodeId))
+
 
 
 
@@ -338,7 +379,8 @@ if __name__ == '__main__':
         "apiKey": apiKey,
     }
 
-    main(ANiMeUrl, apiKey)
+    # [88287, 425998]
+    main(ANiMeUrl, apiKey, [])
 
     api = HX_ANiMe_Api(ANiMeUrl, HXANiMeHeaders)
     exit()
